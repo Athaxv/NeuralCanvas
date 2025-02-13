6482da208f40c24681cd76b9c0497d1277298edb
AtharvGaur <atharava112@gmail.com>
2025-02-13 23:26:22 +0530

    setted up auth part

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5'}
+    }}>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
